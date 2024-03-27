260bac84e0f791310cfc3bab6cbd511c87261c52
Joseph Lee <130028511+joeylee08@users.noreply.github.com>
2024-03-27 08:34:52 -0700

    did a lot

---
+++ client/src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  
+  return (
+    <div className='footer'>
+      <p id='footerText'>"A journey of a thousand miles begins with a single step."</p>
+    </div>
+  )
+}
+
+export default Footer;
--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import NavBar from '../components/NavBar'
 import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 
 const Contact = () => {
 
@@ -8,6 +9,22 @@ const Contact = () => {
     <>
       <Banner />
       <NavBar />
+      <div className='content'>
+        <h1>Contact our team today.</h1>
+        <p>Please do not hesitate to reach out to our team with as much or as little detail as you feel comfortable sharing. We will generally get back to you within 24 hours. Thank you.</p>
+        <div className='formBox'>
+          <form className='contactForm'>
+            <label htmlFor='email'>Email: </label>
+            <input id='email' type='text'></input>
+            <br/>
+            <label htmlFor='notes'>Notes: </label>
+            <textarea id='notes' type='text'></textarea>
+            <br/>
+            <button id='submit' type='submit'>SUBMIT</button>
+          </form>
+        </div>
+      </div>
+      <Footer />
     </>
   )
 }
